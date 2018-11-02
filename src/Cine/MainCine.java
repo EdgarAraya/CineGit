6af92cce0330073f17d3331832c39e518e7a55ab
ed <noted@192.168.0.4>
2018-11-02 20:25:24 -0300

    test laptop7: SetupCine in Main

--- src/Cine/MainCine.java
+++ src/Cine/MainCine.java
@@ -47,22 +47,75 @@ public class MainCine {
     //}
     
     public static Cine setupCine(){
-        String nombreCine= new String();
         Scanner reader= new Scanner(System.in);
+        String nombreCine= new String();
+        String nombreSala= new String();
+        ArrayList<String> nombresSala= new ArrayList();
+        Sala sala;
+        int numeroSalas;
+        int valorEntrada;
+        int numeroFilas;
+        int numeroColumnas;
+        
         
         do {
-            System.out.println("Ingrese nombre del cine:\n");
+            System.out.println("Ingrese nombre del cine:");
             nombreCine= reader.nextLine();
             
         } while (nombreCine!=null && nombreCine.trim().isEmpty());
-        
-        
-            
-        
-        
-        
-        
+         
         Cine cine = new Cine(nombreCine);
+        
+        do{
+            System.out.println("El numero maximo de salas que puede tener el cine es 9.");
+            System.out.println("Ingrese numero de salas del cine:");
+            numeroSalas= Integer.parseInt(reader.nextLine());
+        } while(numeroSalas<=0 || numeroSalas>10);
+        
+        
+        for (int i = 0 ; i< numeroSalas;i++){
+            do{
+                System.out.println("Ingrese nombre de sala "+(i+1));
+                nombreSala= reader.nextLine();
+
+            }while(nombreSala!=null && nombreSala.trim().isEmpty() || nombresSala.contains(nombreSala));
+        
+            nombresSala.add(nombreSala); 
+            do {
+                System.out.println("Ingrese valor de entrada de sala "+nombreSala);
+                valorEntrada= Integer.parseInt(reader.nextLine());
+            }while(valorEntrada<1000 || valorEntrada >32767);
+            
+            do{
+                System.out.println("Ingrese numero de filas de la sala "+nombreSala);
+                numeroFilas= Integer.parseInt(reader.nextLine());
+            } while(numeroFilas<3 || numeroFilas>15);
+            
+            do{
+                System.out.println("Advertencia: Debido a especificaciones del diseno del programa,");
+                System.out.println("la cantidad de asientos no puede superar 127 (valor maximo de byte),");
+                System.out.println("por lo que dado el numero actual de filas, SE RECOMIENDA como");
+                System.out.print("valor maximo de columnas : ");
+                if (127/numeroFilas>15){
+                    System.out.print("15");
+                }else{
+                    System.out.print((int)(127/numeroFilas));
+                }
+                System.out.println("\nIngrese numero de columnas de la sala "+nombreSala);
+                numeroColumnas=Integer.parseInt(reader.nextLine());
+            }while (numeroColumnas<5 && numeroColumnas>12);
+            
+            sala = new Sala(nombreSala,(byte) numeroFilas,(byte) numeroColumnas,(short) valorEntrada);
+            cine.agregarSala(sala);
+            
+            
+            
+        }
+        
+        
+        
+        
+        
         
         return cine;
         
